eac087e4b40dc61927670160dc55f2f539bcca4c
--local <--local>
2025-02-22 12:33:32 +0100

    Addin yoga 🧘‍♂️ and database seed 🌱

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello world!");
+import { createServer } from 'node:http';
+import { createYoga, createSchema } from 'graphql-yoga';
+
+
+const port = Number(process.env.PORT) || 4000;
+const yoga = createYoga({
+    schema: createSchema({
+        typeDefs: /* GraphQL */ `
+            type Query {
+                hello: String!
+            }
+        `,
+        resolvers: {
+            Query: {
+                hello: () => 'Hello World!',
+            },
+        },
+    })
+});
+
+const server = createServer(yoga);
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port} 🚀`)
+});
