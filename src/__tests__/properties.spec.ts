245e2c13607b77689427026ce9ea860154fb9706
--local <--local>
2025-02-23 13:53:51 +0100

    Setup of integration tests complete ⨑🧪

--- src/__tests__/properties.spec.ts
+++ src/__tests__/properties.spec.ts
@@ -4,29 +4,33 @@ import { executor } from "./helpers/setup";
 import { parse } from "graphql";
 
 describe("properties", () => {
-  test("getProperties", async () => {
-    console.log("ENV TEST DATABASE URL", process.env.DATABASE_URL);
-    const query = parse(`
-      query {
-        properties {
-          id
-          street
-          city
-          zip
-          state
-          weatherData {
-            temperature
-            windSpeed
-          }
-          coordinates {
-            latitude
-            longitude
-          }
-          createdAt
-      }
+  const propertiesQuery = parse(`
+    query {
+      properties {
+        id
+        street
+        city
+        zip
+        state
+        weatherData {
+          temperature
+          windSpeed
+        }
+        coordinates {
+          latitude
+          longitude
+        }
+        createdAt
     }
-    `);
-    const result = await executor({ document: query });
-    expect(result).toMatchSnapshot();
+  }
+  `);
+
+  test("Get empty list when the database is empty", async () => {
+    const result = await executor({ document: propertiesQuery });
+    expect(result).toEqual({
+      data: {
+        properties: [],
+      },
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-import { createServer } from "node:http";
-import { createYoga } from "graphql-yoga";
-import { schema } from "./property/schema/schema";
+import main from "./app";
 
-const url = process.env.SERVER_URL || "http://localhost";
-const port = Number(process.env.PORT) || 4000;
-const yoga = createYoga({
-  schema,
-});
-
-const server = createServer(yoga);
-
-server.listen(port, () => {
-  console.log(`Server is running on ${url}:${port} 🚀`);
-});
+main();
